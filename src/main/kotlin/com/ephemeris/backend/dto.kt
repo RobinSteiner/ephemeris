e3c755df62b736b77b2052a68a92ed8ecd39bd90
Robin Steiner <Robin.Steiner@catalysts.cc>
2017-09-09 14:55:14 +0200

    finished dtos

--- src/main/kotlin/com/ephemeris/backend/dto.kt
+++ src/main/kotlin/com/ephemeris/backend/dto.kt
@@ -2,33 +2,31 @@ package com.ephemeris.backend
 
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
-import com.fasterxml.jackson.annotation.JsonTypeName
-import java.awt.Color
 import java.util.*
-import kotlin.reflect.KClass
 
-@JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include= JsonTypeInfo.As.WRAPPER_OBJECT, property="type")
-@JsonSubTypes({
-
-    @JsonSubTypes.Type(value = TextCard::class, name = "text_card"),
-
-    @JsonSubTypes.Type(value=ImageCard::class, name="cat")
-
-})
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type", visible = true)
+@JsonSubTypes(
+    JsonSubTypes.Type(value = TextCard::class, name = "text_card"),
+    JsonSubTypes.Type(value = ImageCard::class, name = "image_card"),
+    JsonSubTypes.Type(value = VideoCard::class, name = "video_card"),
+    JsonSubTypes.Type(value = YoutubeCard::class, name = "youtube_card"),
+    JsonSubTypes.Type(value = MusicCard::class, name = "music_card"),
+    JsonSubTypes.Type(value = SpotifyCard::class, name = "spotify_card"),
+    JsonSubTypes.Type(value = LocationCard::class, name = "location_card")
+)
 interface Card {
     val date: Date
     val order: Long
 }
 
-enum class Theme{ Lite, Dark }
-enum class LocationMode{ Place, Map }
-enum class MusicMode{ Big, Small }
+enum class Theme { Lite, Dark }
+enum class LocationMode { Place, Map }
+enum class MusicMode { Big, Small }
 
-@JsonTypeName("text_card")
-data class TextCard(val test: String, val title: String, val backgroundColor: Color, val foregroundColor: Color, override val date: Date, override val order: Long): Card
-data class ImageCard(val source: String, override val date: Date, override val order: Long): Card
-data class VideoCard(val source: String, override val date: Date, override val order: Long): Card
-data class YoutubeCard(val id: String, override val date: Date, override val order: Long): Card
-data class MusicCard(val source: String, override val date: Date, override val order: Long): Card
-data class SpotifyCard(val id: String, val theme: Theme, val mode: MusicMode, override val date: Date, override val order: Long): Card
-data class LocationCard(val longitude: Float, val latitude: Float, val place: String, val mode: LocationMode, override val date: Date, override val order: Long): Card
+data class TextCard(val text: String = "", val title: String = "", val backgroundColor: String = "#ffffff", val foregroundColor: String = "#000000", override val date: Date = Date(), override val order: Long = 0) : Card
+data class ImageCard(val source: String = "", override val date: Date = Date(), override val order: Long = 0) : Card
+data class VideoCard(val source: String = "", override val date: Date = Date(), override val order: Long = 0) : Card
+data class YoutubeCard(val id: String = "", override val date: Date = Date(), override val order: Long = 0) : Card
+data class MusicCard(val source: String = "", override val date: Date = Date(), override val order: Long = 0) : Card
+data class SpotifyCard(val id: String = "", val theme: Theme = Theme.Lite, val mode: MusicMode = MusicMode.Small, override val date: Date = Date(), override val order: Long = 0) : Card
+data class LocationCard(val longitude: Float = 0F, val latitude: Float = 0F, val place: String = "", val mode: LocationMode = LocationMode.Map, override val date: Date = Date(), override val order: Long = 0) : Card
